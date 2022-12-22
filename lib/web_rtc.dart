504faa985561656d8ba5dd9e8407a3ec764da7f9
MISEONGKIM <rlaaltjd46@jinhakapply.com>
2022-12-22 12:07:38 +0900

    'first'

---
+++ lib/web_rtc.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WebRtcPage extends StatefulWidget {
+  const WebRtcPage({Key? key}) : super(key: key);
+
+  @override
+  _WebRtcPageState createState() => _WebRtcPageState();
+}
+
+class _WebRtcPageState extends State<WebRtcPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('webRtc '),
+    );
+  }
+}
